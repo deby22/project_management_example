788a5df04b3c0d7399a7e4691beee2d25bad31be
Dawid Dęby <dawid.deby@gmail.com>
2021-01-25 11:13:13 +0100

    used json-api instead of api

--- lib/project_management_example_web/router.ex
+++ lib/project_management_example_web/router.ex
@@ -13,9 +13,14 @@ defmodule ProjectManagementExampleWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :json_api do
+    plug :accepts, ["json-api"]
+    plug JaSerializer.Deserializer
+  end
+
   # Other scopes may use custom stacks.
   scope "/api", ProjectManagementExampleWeb do
-    pipe_through :api
+    pipe_through :json_api
 
     resources "/projects", ProjectController, only: [:index, :show]
     resources "/documents", DocumentController, only: [:index, :show, :create]
